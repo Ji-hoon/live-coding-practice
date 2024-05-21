d761caa5561c9387e0595ab1a03fd2e7d8ad3a61
[SW7] 김지훈 <lanslot84@gmail.com>
2024-05-21 18:28:19 +0900

    Chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import SignUp from "./components/pages/SignUp";
+import Todos from "./components/pages/Todos";
+
+function App() {
+  return (
+    <>
+      <Todos />
+    </>
+  );
+}
+
+export default App;
