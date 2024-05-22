1de9f515266ef01572abff6a80f573cf20e22cdf
[SW7] 김지훈 <lanslot84@gmail.com>
2024-05-22 12:53:05 +0900

    Chore: tailwind 설정 파일 추가

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const Config = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default Config;
